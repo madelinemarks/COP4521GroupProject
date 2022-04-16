d14c1c293f54e92eeff92514e0e1b9b0be46edad
areid31 <akeilareid12@gmail.com>
2022-04-15 20:22:06 -0400

    fixing my last commit to merge with the new app.py

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, url_for, redirect, flash
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from werkzeug.security import generate_password_hash, check_password_hash
+from os import environ
 import sqlite3 as sql
 import sys
 import time
+import stripe
 app = Flask(__name__)
+
+app.config['STRIPE_PUBLIC_KEY'] = environ.get("STRIPE_PUBLIC_KEY")
+app.config['STRIPE_PRIVATE_KEY'] = environ.get("STRIPE_PRIVATE_KEY")
+stripe.api_key = app.config['STRIPE_PRIVATE_KEY']
 
 @app.route('/')
 def home():
-    return render_template('index.html', template_folder = 'Templates')
+    return render_template('index.html', template_folder='Templates')
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html', template_folder = 'Templates')
+    return render_template('signup.html', template_folder='Templates')
 
 @app.route('/addUser', methods = ['POST', 'GET'])
 def addUser():
@@ -55,7 +61,7 @@ def userInfo():
 
 @app.route('/loginPage')
 def loginPage():
-    return render_template('loginPage.html', template_folder = 'Templates')
+    return render_template('loginPage.html', template_folder='Templates')
 
 @app.route('/loginAttempt', methods = ['POST', 'GET'])
 def loginAttempt():
@@ -87,12 +93,12 @@ def loginAttempt():
 
 @app.route('/search')
 def search():
-    return render_template('search.html', template_folder = 'Templates')
+    return render_template('search.html', template_folder='Templates')
 
-@app.route('/searchResults', methods = ['POST', 'GET'])
+@app.route('/searchResults', methods=['POST', 'GET'])
 def searchResults():
     if request.method == 'POST':
-        #start with empty dbRows tuple
+        # start with empty dbRows tuple
         dbRows = ()
         try:
             query = request.form['query']
@@ -102,19 +108,19 @@ def searchResults():
                 cur = con.cursor()
                 con.row_factory = sql.Row
 
-                #add rows found by isbn from database
+                # add rows found by isbn from database
                 if type == "isbn":
                     msg = "Searching for ISBN#" + query
                     cur.execute("SELECT * FROM Textbooks WHERE ISBN = ?", (query))
                     dbRows = cur.fetchall()
 
-                #add rows found by title from database
+                # add rows found by title from database
                 elif type == "title":
                     msg = "Searching for \"" + query + "\" by title"
                     cur.execute("SELECT * FROM Textbooks WHERE Title = ?", (query))
                     dbRows = cur.fetchall()
 
-                #no variable defined
+                # no variable defined
                 else:
                     msg = "Search failed, invalid query."
 
@@ -123,43 +129,44 @@ def searchResults():
         finally:
             con.close()
 
-            #create dictionary to find scraped web prices
-            webRows=[]
-            #load chromedriver from local chromedriver installation
+            # create dictionary to find scraped web prices
+            webRows = []
+            # load chromedriver from local chromedriver installation
             s = Service('/Users/malry/Desktop/classes/python/chromedriver')
             driver = webdriver.Chrome(service=s)
-            #use driver to get info from book aggregator
+            # use driver to get info from book aggregator
             driver.get('https://www.bookfinder.com/')
             if (type == "isbn"):
                 isbnInput = driver.find_element(By.NAME, 'isbn').send_keys(query)
                 isbnSubmit = driver.find_element(By.ID, 'submitBtn').click()
-                time.sleep(5) #TODO REMOVE, only for testing screenshot capability
+                time.sleep(5)  # TODO REMOVE, only for testing screenshot capability
                 driver.save_screenshot('isbnScreen.png')
             elif (type == "title"):
                 titleInput = driver.find_element(By.ID, 'title').send_keys(query)
                 titleSubmit = driver.find_element(By.ID, 'submitBtn').click()
-                time.sleep(5) #TODO REMOVE, only for testing screenshot capability
+                time.sleep(5)  # TODO REMOVE, only for testing screenshot capability
                 driver.save_screenshot('titleScreen.png')
-            #create list to include results from competitor websites (amazon to start test)
-            return render_template('searchResults.html', template_folder = 'Templates', msg=msg, query=query, type=type, rows=dbRows, webRows=webRows)
+            # create list to include results from competitor websites (amazon to start test)
+            return render_template('searchResults.html', template_folder='Templates', msg=msg, query=query, type=type,
+                                   rows=dbRows, webRows=webRows)
 
 @app.route('/createListing')
 def createListing():
-    return render_template('createListing.html', template_folder = 'Templates')
+    return render_template('createListing.html', template_folder='Templates')
 
-@app.route('/addListing', methods = ['POST', 'GET'])
+@app.route('/addListing', methods=['POST', 'GET'])
 def addListing():
     if request.method == 'POST':
         con = sql.connect("siteData.db")
         msg = "Not at try"
         try:
-            title   = request.form['title']
-            isbn    = request.form['isbn']
-            askprc  = request.form['askprc']
+            title = request.form['title']
+            isbn = request.form['isbn']
+            askprc = request.form['askprc']
             msg = "Finished try"
 
             cur = con.cursor()
-            cur.execute("INSERT INTO Listings (Title, ISBN, Asking, HighestBid) VALUES (?,?,?,?)", (title,isbn,askprc,0,))
+            cur.execute("INSERT INTO Listings (Title, ISBN, Asking, HighestBid) VALUES (?,?,?,?)",(title, isbn, askprc, 0,))
             con.commit()
             msg = "Successfully created listing"
         except:
@@ -168,14 +175,49 @@ def addListing():
             con.close()
             return result(msg)
 
-
 @app.route('/result')
 def result(msg):
-    return render_template('result.html', template_folder = 'Templates', msg = msg)
+    return render_template('result.html', template_folder='Templates', msg=msg)
 
 @app.route('/createTextbook')
 def createTextbook():
-    return render_template('createTextbook.html', template_folder = 'Templates')
+    return render_template('createTextbook.html', template_folder='Templates')
 
-if __name__ == '__main__':      # main
-   app.run(debug = True)
+@app.route('/listings')
+def listings():
+    con = sql.connect("siteData.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Listings")
+
+    rows = cur.fetchall()
+    return render_template("listings.html", rows=rows)
+
+@app.route('/checkout', methods=['POST'])
+def checkout():
+    title = request.form['title']
+    isbn = request.form['isbn']
+    asking = int(float(request.form['asking'].replace('$', ''))) * 100
+
+    session = stripe.checkout.Session.create(
+        line_items=[{
+            'name': title,
+            'amount': asking,
+            'quantity': 1,
+            'currency': 'usd',
+            'description': 'ISBN: ' + isbn
+        }],
+        payment_method_types=['card'],
+        mode='payment',
+        success_url=url_for('success', _external=True),  # + '?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url=url_for('listings', _external=True)
+    )
+    return redirect(session.url, code=303)
+
+@app.route('/success')
+def success():
+    return render_template("success.html")
+
+if __name__ == '__main__':  # main
+    app.run(debug=True)
