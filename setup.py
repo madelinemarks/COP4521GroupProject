748f20acfa3ddd856152b93d69885a3e0d7d3c72
madelinemarks <60200895+madelinemarks@users.noreply.github.com>
2022-04-18 22:18:42 -0400

    Updated setup and app with user roles

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ conn = sqlite3.connect('siteData.db')   # do we need more than one database?
 print("Opened database successfully")
 
 # we will definitely need to add more tables and change data types, placeholders for now
-conn.execute('CREATE TABLE Users(Username TEXT, FirstName TEXT, LastName TEXT, DOB DATE, Pass TEXT)') #  we should probably store passwords differently, how to store Date?
+conn.execute('CREATE TABLE Users(Username TEXT, FirstName TEXT, LastName TEXT, DOB DATE, Pass TEXT, UserType TEXT)') #  we should probably store passwords differently, how to store Date?
 conn.execute('CREATE TABLE Textbooks(Title TEXT, ISBN TEXT, Retail REAL, Subj TEXT)')
 conn.execute('CREATE TABLE Listings(Title TEXT, ISBN TEXT, Asking REAL, HighestBid REAL)')
