0275bebbaa565b65bf1fca51946344dbd9acc927
malry <58125586+malryt@users.noreply.github.com>
2022-04-20 19:39:59 -0400

    ISBN Scraping Finished

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, url_for, redirect, flash
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 from werkzeug.security import generate_password_hash, check_password_hash
 from os import environ
 import sqlite3 as sql
@@ -44,10 +47,10 @@ def addUser():
                         con.commit()
                         msg = "Successfully created user"
             except:
-             con.rollback()
+                con.rollback()
             finally:
-                return result(msg)
                 con.close()
+                return result(msg)
 
 @app.route('/userTable')
 def userInfo():
@@ -57,7 +60,7 @@ def userInfo():
     cur = con.cursor()
     cur.execute("SELECT * FROM Users")
 
-    rows = cur.fetchall();
+    rows = cur.fetchall()
     return render_template('userTable.html', rows=rows)
 
 @app.route('/loginPage')
@@ -112,13 +115,13 @@ def searchResults():
                 # add rows found by isbn from database
                 if type == "isbn":
                     msg = "Searching for ISBN#" + query
-                    cur.execute("SELECT * FROM Textbooks WHERE ISBN = ?", (query))
+                    cur.execute("SELECT * FROM Listings WHERE ISBN = ?", (query))
                     dbRows = cur.fetchall()
 
                 # add rows found by title from database
                 elif type == "title":
                     msg = "Searching for \"" + query + "\" by title"
-                    cur.execute("SELECT * FROM Textbooks WHERE Title = ?", (query))
+                    cur.execute("SELECT * FROM Listings WHERE Title = ?", (query))
                     dbRows = cur.fetchall()
 
                 # no variable defined
@@ -128,26 +131,46 @@ def searchResults():
         except:
             con.rollback()
         finally:
-            con.close()
-
-            # create dictionary to find scraped web prices
-            webRows = []
             # load chromedriver from local chromedriver installation
-            s = Service('/Users/madelinemarks/Downloads/chromedriver')
-            driver = webdriver.Chrome(service=s)
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+            wait = WebDriverWait(driver, 15)
             # use driver to get info from book aggregator
             driver.get('https://www.bookfinder.com/')
             if (type == "isbn"):
+                #submit ISBN input
                 isbnInput = driver.find_element(By.NAME, 'isbn').send_keys(query)
                 isbnSubmit = driver.find_element(By.ID, 'submitBtn').click()
-                time.sleep(5)  # TODO REMOVE, only for testing screenshot capability
-                driver.save_screenshot('isbnScreen.png')
+
+                #find lowest price (new)
+                e = wait.until(EC.visibility_of_element_located((By.ID, 'describe-isbn-title')))
+                title = e.get_attribute('innerHTML')
+
+                e = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="bd-isbn"]/div/table/tbody/tr/td[1]/table/tbody/tr[2]/td[4]/div/span/a')))
+                lowestNew = e.get_attribute('text')
+
+                newLink = driver.find_element(By.XPATH, '//*[@id="bd-isbn"]/div/table/tbody/tr/td[1]/table/tbody/tr[2]/td[4]/div/span/a').get_attribute('href')
+                sellerInfo = str(newLink)
+
+                #TODO remove once final testing screenshot to ensure correct found
+                #driver.save_screenshot('isbnScreen.png')
+
+                print( "Book Info: " + str(title) + str(query) + str(lowestNew) + str(sellerInfo), flush=True)
+                #insert into Textbooks if not found, update if found
+                cur.execute("INSERT OR IGNORE INTO Textbooks (Title, ISBN, Retail, Subj) VALUES (?, ?, ?, ?)", (title, query, lowestNew, sellerInfo,))
+                cur.execute("UPDATE Textbooks SET Retail = ? WHERE ISBN = ?", (lowestNew, query,))
+                cur.execute("SELECT * FROM Textbooks WHERE ISBN = ?" , (query,))
+                webRows = cur.fetchall()
+                con.commit()
+                print (webRows, flush=True)
+
             elif (type == "title"):
                 titleInput = driver.find_element(By.ID, 'title').send_keys(query)
                 titleSubmit = driver.find_element(By.ID, 'submitBtn').click()
                 time.sleep(5)  # TODO REMOVE, only for testing screenshot capability
                 driver.save_screenshot('titleScreen.png')
             # create list to include results from competitor websites (amazon to start test)
+            cur.close()
+            con.close()
             return render_template('searchResults.html', template_folder='Templates', msg=msg, query=query, type=type,rows=dbRows, webRows=webRows)
 
 @app.route('/createListing')
